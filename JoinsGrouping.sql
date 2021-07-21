2297eecf7b3997a8df94357cea236a22ee573f30
Kyle Conway <k.conwayw@gmail.com>
2021-07-21 13:29:14 -0500

    finished work

--- JoinsGrouping.sql
+++ JoinsGrouping.sql
@@ -24,5 +24,16 @@ SELECT productLine as 'Product Line', SUM(o.quantityOrdered) AS '# Sold'
   ORDER BY SUM(o.quantityOrdered) desc;
   
 -- 5.
+SELECT 
+    CONCAT(e.lastName, ', ', e.firstName) AS 'Sales Rep', count(quantityOrdered) AS '# Orders', SUM(od.quantityOrdered * od.priceEach) AS 'Total Sales'
+    FROM orderdetails od
+    JOIN orders o ON o.orderNumber = od.orderNumber
+    JOIN customers c ON c.customerNumber = o.customerNumber
+    JOIN employees e ON e.employeeNumber = c.salesRepEmployeeNumber
+    GROUP BY e.lastName;
 
 -- 6.
+SELECT MONTHNAME(paymentDate) AS 'Month', YEAR(paymentDate) AS 'Year', FORMAT(SUM(amount), 2) AS 'Payments Recieved'
+  FROM payments
+  GROUP BY month(paymentDate), year(paymentDate)
+  ORDER BY year(paymentDate), month(paymentDate)
